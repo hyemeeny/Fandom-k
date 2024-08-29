bbbd2cb82bb18ec4c8384866a0c5939f41341b64
JeongminSeon <skelters@naver.com>
2024-08-29 14:30:55 +0900

    💩 Added RadioButton, RadioButtonGroup Component
    Added Components considering reusability Closes #12

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 /** @jsxImportSource @emotion/react */
 import { Route, Routes } from "react-router-dom";
 import GlobalStyles from "./styles/global";
-import RadioButton from "./components/RadioButton";
 
 function App() {
   return (
     <>
       <GlobalStyles />
-      {/* <Routes>
+      <Routes>
         <Route path="/" />
         <Route path="/list" />
         <Route path="/mypage" />
-      </Routes> */}
-      <RadioButton />
+      </Routes>
     </>
   );
 }
